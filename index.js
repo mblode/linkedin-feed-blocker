10629e2189f14b3f80ea295fd4c45502d94ba804
Matthew Blode <m@blode.co>
2019-10-18 11:35:39 +1100

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const feedID = document.getElementById('voyager-feed');
+const mainId = feedID.getElementsByClassName('core-rail')[0];
+const contentId = mainId.getElementsByClassName('ember-view')[1];
+contentId.style.display = 'none';
